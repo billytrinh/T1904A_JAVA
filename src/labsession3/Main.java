e5ccf042bcd63ec05c99fcbb9cefa2e9583bec44
quanghoa.trinh@gmail.com <quanghoa.trinh@gmail.com>
2019-10-11 17:19:13 +0700

    lab

---
+++ src/labsession3/Main.java
@@ -0,0 +1,16 @@
+package labsession3;
+
+public class Main {
+    public static void main(String args[]){
+        Fraction fc = new Fraction();
+        fc.nhapPhanSo();
+        fc.inPhanSo();
+        fc.rutGonPhanSo();
+        fc.inPhanSo();
+        fc.nghichDaoPhanSo();
+        fc.inPhanSo();
+        Fraction fc2 = new Fraction(1,3);
+
+        Fraction tong = fc.addFraction(fc2);
+    }
+}
